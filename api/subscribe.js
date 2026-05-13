d22ebbaaef47da701c32c62e29b2eff025b266cc
Jason Weaver <mixdown04@gmail.com>
2026-05-13 15:24:46 -0500

    Update audience ID in subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const { email } = req.body;
   if (!email) return res.status(400).json({ error: 'Email required' });
 
-  const response = await fetch('https://api.resend.com/audiences/PASTE_YOUR_AUDIENCE_ID_HERE/contacts', {
+  const response = await fetch('https://api.resend.com/audiences/03356efd-7b0f-465e-b2d8-7934246d5d68/contacts', {
     method: 'POST',
     headers: {
       'Authorization': `Bearer ${process.env.RESEND_API_KEY}`,
